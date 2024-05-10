2312f152bb165844bea7c9f75efbfca1cc581b39
Gustavo Bonemann <gustavobonemann@gmail.com>
2024-05-10 18:53:28 -0300

    feat: hide api key

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { GlobalStyle } from './global-styles/globalstyle'
 import { MainContainer } from './components/MainContainer'
 import { Header } from './components/Header'
+import { api } from './services/api'
+import { apiKey } from '../api-key'
 
 function App() {
   const [count, setCount] = useState(0)
+  useEffect(() => {
+    const getComics = async () => {
+      const { data } = await api.get(`/comics?apikey=${apiKey}`);
+      console.log(data.data.results)
+    }
+    getComics();
+  }, [])
 
   return (
     <>
