7293567a8917936ea8a5b7bab984f8aa6ecc2580
Gustavo Bonemann <gustavobonemann@gmail.com>
2024-05-10 22:32:51 -0300

    feat: applied roboto font

--- src/global-styles/globalstyle.js
+++ src/global-styles/globalstyle.js
@@ -6,6 +6,7 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         list-style-type: none;
+        font-family: "Roboto", sans-serif;
     }
 
     body {
