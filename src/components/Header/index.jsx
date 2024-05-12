4116933d8a003ff6777e34dcd09a94d4b3e65730
Gustavo Bonemann <gustavobonemann@gmail.com>
2024-05-12 01:37:15 -0300

    fix: marvel icon in header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,13 +2,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { StyledHeader } from "./style"
 import { faCartShopping, faHouse } from "@fortawesome/free-solid-svg-icons"
 import { Link } from "react-router-dom"
+import marvel_icon from "../../assets/marvel_icon.jpg";
 
 export const Header = ({ toggleModal }) => {
     return (
         <StyledHeader>
             <div>
                 <Link to="/">
-                    <img src=".././src/assets/marvel-icon.jpg" alt="Marvel Icon" />
+                    <img src={marvel_icon} alt="Marvel Icon" />
                 </Link>
                 <nav>
                     <button onClick={toggleModal}>
