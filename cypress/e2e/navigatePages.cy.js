f9a14204a923eea11f32e520496d18e67d08b7e7
Gustavo Bonemann <gustavobonemann@gmail.com>
2024-05-12 19:56:12 -0300

    feat: update page navigation test

--- cypress/e2e/navigatePages.cy.js
+++ cypress/e2e/navigatePages.cy.js
@@ -1,18 +1,22 @@
 describe('navigation pages', () => {
   it('should be navigate pages correctly', () => {
     cy.visit('/')
+
+    const nextButton = cy.get('section div button')
+      .eq(1);
+
+    const previousButton = cy.get('section div button')
+      .eq(0);
+
     cy.get('section > span').should('contain.text', '1 de');
 
-    cy.get('section div button')
-      .eq(1)
-      .click();
-    
+    previousButton.click();
+    cy.get('section > span').should('contain.text', '1 de');
+
+    nextButton.click();
     cy.get('section > span').should('contain.text', '2 de');
 
-    cy.get('section div button')
-      .eq(0)
-      .click();
-
+    previousButton.click();
     cy.get('section > span').should('contain.text', '1 de');
 
     let title;
@@ -20,21 +24,14 @@ describe('navigation pages', () => {
       title = $h2.get(0).innerText;
     })
 
-    cy.get('section div button')
-      .eq(1)
-      .click();
-
+    nextButton.click();
     cy.get('li h2').should(($h2) => {
       expect($h2.get(0).innerText).to.not.eq(title);
     })
-    for(let i = 0; i < 19; i++) {
-      cy.get('section div button')
-        .eq(1)
-        .click()
-    }
 
-    cy.on('window:alert', (str) => {
-      expect(str).to.equal("Página indisponível!")
-    })
+    for(let i = 0; i < 19; i++) {
+      nextButton.click()
+    }
+    cy.get('section > span').should('contain.text', '20 de');
   })
 })
