23f72edb72804590769f1121cf718b7a8b95a0b7
Gustavo Bonemann <gustavobonemann@gmail.com>
2024-05-12 20:50:37 -0300

    feat: detail page test

---
+++ cypress/e2e/viewDetails.cy.js
@@ -0,0 +1,36 @@
+describe('view details', () => {
+  it('should be navigate pages correctly', () => {
+    cy.visit('/');
+
+    let title;
+    cy.get('li h2').should(($h2) => {
+      title = $h2.get(0).innerText;
+    })
+
+    cy.get('li button')
+      .eq(0)
+      .click();
+
+    cy.url().should('include', '/view/');
+
+    cy.get('h1').should(($h1) => {
+      expect($h1.get(0).innerText).to.eq(title);
+    })
+
+    cy.get('header img').click();
+
+    cy.url().should('not.include', '/view');
+
+    cy.get('li button')
+      .eq(0)
+      .click();
+
+    cy.url().should('include', '/view/');
+    
+    cy.get('nav button')
+      .eq(1)
+      .click();
+    
+    cy.url().should('not.include', '/view');
+  })
+})
