c6b9abde2c91380d1058032c7de0a1a9fd03a195
Gustavo Bonemann <gustavobonemann@gmail.com>
2024-05-12 22:35:23 -0300

    feat: added random number generator to context

--- src/providers/ComicContext.jsx
+++ src/providers/ComicContext.jsx
@@ -10,7 +10,27 @@ export const ComicProvider = ({ children }) => {
         if(!localStorage.getItem("comicList")) {
             const { data } = await api.get(`/comics?format=comic&formatType=comic&noVariants=false&dateDescriptor=thisMonth&limit=80&apikey=${import.meta.env.VITE_API_KEY}`);
             const list = data.data.results;
+
+            const max = list.length / 10;
+            const numbers = [];
+            for(let i = 0; i < max; i++) {
+                const generateNumber = () => {
+                    let number = Math.round(Math.random() * (list.length - 1));
+                    
+                    if(numbers.includes(number)) {
+                        number = generateNumber();
+                    }
+
+                    console.log(number);
+                    return number;
+                }
+                
+                numbers.push(generateNumber());
+            }
+            console.log(numbers);
+
             setComicList(list);
+            
             localStorage.setItem("comicList", JSON.stringify(list));
         } else {
             setComicList(JSON.parse(localStorage.getItem("comicList")))
